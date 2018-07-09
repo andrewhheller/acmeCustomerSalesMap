0ae3904bb5b67fda38006fba4f1f13b3c2ae97cc
Andrew Heller <andrewhheller@gmail.com>
2018-07-09 15:14:20 -0400

    add project files

---
+++ acmesales.js
@@ -0,0 +1,55 @@
+const sales = [
+  {customerId: 1,
+  orderId: 1,
+  total: 3},
+  {customerId: 2,
+  orderId: 2,
+  total: 4},
+  {customerId: 1,
+  orderId: 2,
+  total: 7},
+];
+
+const customers = [
+  {id: 1,
+  name: 'moe'},
+  {id: 2,
+  name: 'curly'},
+  {id: 3,
+  name: 'larry'}
+];
+
+function generateCustomerSalesMap(sales, customers) {
+
+  const combinerTotalWrapper = (id) => {
+
+    const combinerTotal = (acc, ele, idx) => {
+      if(ele.customerId === id) {
+        acc += ele.total;
+      }
+
+      return acc;
+    }
+
+    return sales.reduce(combinerTotal, 0);
+  }
+
+  // combiner function for reduce
+  const combinerCustomerSalesMap = (acc, ele, idx) => {
+
+    const customerId = ele.id;
+
+    // create key/value in accumulator {}
+    // {}[customer name] = sales[element index].total
+    acc[ele.name] = combinerTotalWrapper(customerId);
+
+    // return populated object
+    return acc;
+  }
+
+  return customers.reduce(combinerCustomerSalesMap, {});
+}
+  
+console.log(generateCustomerSalesMap(sales, customers));
+
+module.exports = generateCustomerSalesMap;
---
+++ test/acmesales-specs.js
@@ -0,0 +1,68 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const generateCustomerSalesMap = require('../acmesales');
+
+describe('acme sales: generate customer sales map', () => {
+
+  let salesTest;
+    
+  let customersTest;
+
+  let result;
+
+  let customerTotal;
+
+  beforeEach(() => {
+
+    salesTest = [
+      {customerId: 1,
+      orderId: 1,
+      total: 3},
+      {customerId: 2,
+      orderId: 2,
+      total: 4},
+      {customerId: 2,
+      orderId: 2,
+      total: 7},
+    ];
+
+    customersTest = [
+      {id: 1,
+      name: 'Jean-Luc'},
+      {id: 2,
+      name: 'William'},
+      {id: 3,
+      name: 'Data'}
+    ];
+
+    result = generateCustomerSalesMap(salesTest, customersTest);
+
+    customerTotal = Object.keys(result).length;
+
+  })
+
+  it('is a function', () => {
+    expect(generateCustomerSalesMap).to.be.a('function');
+  });
+
+  it('returns an object', () => {
+    expect(result).to.be.a('object');
+  })
+
+  it('returns the correct amount of customers', () => {
+    let customerSalesMapTotal = Object.keys(result).length;
+    expect(customerSalesMapTotal).to.equal(customerTotal);
+  });
+
+  it('generates the correct order total for each customer', () => {
+    expect(result.William).to.equal(11);
+  });
+
+  it('uses Array.prototype.reduce', () => {
+    const spy = sinon.spy();
+    spy(Array.prototype, 'reduce');
+    expect(spy.called).to.equal(true);
+  })
+
+});
